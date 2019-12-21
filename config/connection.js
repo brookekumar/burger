3590ef6860c81342d4c7ab99ca74bdb6049f291a
Mona Lisa <mrsbrookekumar@gmail.com>
2019-12-21 12:43:49 -0600

    changes to handlebars for burger

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "Chauncey!1",
     database: "burgers_db"
 
   });
